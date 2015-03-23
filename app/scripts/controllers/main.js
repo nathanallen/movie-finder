6c4b9db58bac0db7e8b683a0e77ae470c1d3dd54
nathanallen <thenathanator@gmail.com>
2015-03-23 12:32:20 -0700

    basic skeleton

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,8 @@
+angular.module('movieFinderApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeMovies = [
+      'Looper',
+      'Hackers',
+      'The Internet\'s Own Boy'
+    ];
+  });
